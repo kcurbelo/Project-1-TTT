a0109c7d19004a5f270fe11175779542d58b2612
kcurbelo <kevin.curbelo@gmail.com>
2015-01-23 17:25:21 -0800

    Updated player turn logic- mid fix

--- java.js
+++ java.js
@@ -24,60 +24,16 @@
 //=========================================
 //Other Variables
 //=========================================
-	var playerOne = true;
-	var playerTwo = true; 
+	var player = true; 
 
 	var turnCount = 0;
 
-    var playerIcon = " ";
+
+    
+
+    
 
 //======================== Settign up the turns ========================
-  if(turnCount = 0){
-  	playerTwo = false
-  	console.log("player One Slected")
-    playerIcon = "X"
-
-  }
-  if(turnCount < 1){
-  	playerOne = false
-  	console.log("player Two Slected") 
-  	playerIcon = "O" 
-  }
-  if(turnCount < 2){
-  	playerTwo = false
-    console.log("player One Slected")
-    playerIcon = "X"
-  }
-  if(turnCount < 3){
-  	playerOne = false
-  	console.log("player Two Slected") 
-  	playerIcon = "O"  	   
-  }
-  if(turnCount < 4){
-  	playerTwo = false
-    console.log("player One Slected")
-    playerIcon = "X"
-  }
-  if(turnCount < 5){
-  	playerOne = false
-  	console.log("player Two Slected") 
-  	playerIcon = "O"   	   	
-  }
-  if(turnCount < 6){
-  	playerTwo = false
-  	console.log("player One Slected")
-  	playerIcon = "X"  
-  }
-  if(turnCount < 7){
-  	playerOne = false
-  	console.log("player Two Slected")  	
-  	playerIcon = "O" 
-  }
-  if(turnCount < 8){
-  	playerTwo = false
-  	console.log("player One Slected")
-  	playerIcon = "X"
-  }
 
 //=========================================
 //Adding click to each div
@@ -89,93 +45,53 @@
 
 	document.getElementById("box0").addEventListener("click", function (){
 		console.log("Box0 was clicked");
-		   if(playerOne == true){      //If playerOne is true then change the inner html to "X"
-		       box0.innerHTML = "X";
+		   if(player == true){
+		       box0.innerHTML= "X"
+		       player = false
+		           if (box0 == "X") {
+		           	box0.removeEventListener("click")
+		           };
 		   }
-		   else{
-		   	   box0.innerHTML = "O"    //Else make the inner html an "O"
+
+		   else if(player == false){
+		   	box0.innerHTML = "O"
+		   	player = true
 		   }
-	});
-	document.getElementById("box1").addEventListener("click", function (){
+    });
+    document.getElementById("box1").addEventListener("click", function (){
 		console.log("Box1 was clicked");
-		box1.innerHTML == playerIcon;
-		 if(playerOne == false){       //playerOne is false so do else 
-		       box1.innerHTML = "X";
+		   if(player == true){
+		       box1.innerHTML= "X"
 		   }
-		   else{
-		   	   box1.innerHTML = "O"
+		   else if(player == false){
+		   	box1.innerHTML = "O"
+		   	player = true
 		   }
-	});
+    });
 	document.getElementById("box2").addEventListener("click", function (){
 		console.log("Box2 was clicked");
-		box2.innerHTML == playerIcon;
-		   if(playerOne == true){
-		       box2.innerHTML = "X";
+		   if(player == true){
+		       box2.innerHTML= "X"
+		       player = false
 		   }
-		   else{
-		   	   box2.innerHTML = "O"
+		   else if(player == false){
+		   	box2.innerHTML = "O"
+		   	player = true
 		   }
-	});
-	document.getElementById("box3").addEventListener("click", function (){
+    });
+    document.getElementById("box3").addEventListener("click", function (){
 		console.log("Box3 was clicked");
-		box3.innerHTML = playerIcon;
-		   if(playerOne == false){
-		       box3.innerHTML = "X";
+		   if(player == true){
+		       box3.innerHTML= "X"
 		   }
-		   else{
-		   	   box3.innerHTML = "O"
+		   else if(player == false){
+		   	box3.innerHTML = "O"
+		   	player = true
 		   }
-	});
-	document.getElementById("box4").addEventListener("click", function (){
-		console.log("Box4 was clicked");
-		box4.innerHTML = playerIcon;
-		if(playerOne == true){
-		       box4.innerHTML = "X";
-		   }
-		   else{
-		   	   box4.innerHTML = "O"
-		   }
-	});
-	document.getElementById("box5").addEventListener("click", function (){
-		console.log("Box5 was clicked");
-		box5.innerHTML = playerIcon;
-		if(playerOne == false){
-		       box5.innerHTML = "X";
-		   }
-		   else{
-		   	   box5.innerHTML = "O"
-		   }
-	});
-	document.getElementById("box6").addEventListener("click", function (){
-		console.log("Box6 was clicked");
-		box6.innerHTML = playerIcon;
-		if(playerOne == true){
-		       box6.innerHTML = "X";
-		   }
-		   else{
-		   	   box6.innerHTML = "O"
-		   }
-	});
-	document.getElementById("box7").addEventListener("click", function (){
-		console.log("Box7 was clicked");
-		box7.innerHTML = playerIcon;
-		if(playerOne == false){      
-		       box7.innerHTML = "X";
-		   }
-		   else{
-		   	   box7.innerHTML = "O"
-		   }
-	});
-	document.getElementById("box8").addEventListener("click", function (){
-		console.log("Box8 was clicked");
-		box8.innerHTML = playerIcon;
-		if(playerOne == true){
-		       box8.innerHTML = "X";
-		   }
-		   else{
-		   	   box8.innerHTML = "O"
-		   }
-	});
+    });
+
+
+	
 
 
 // Possibility 1: while playerTurn1 is equal to true allow them to pick a square and once it is clicked turn them to false. 
