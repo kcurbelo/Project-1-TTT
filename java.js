176866d076394cb43263e05c31fe5f28e0049e2b
kcurbelo <kevin.curbelo@gmail.com>
2015-01-24 09:49:12 -0800

    Added x and o turns to all squares

--- java.js
+++ java.js
@@ -21,106 +21,137 @@
 	var box8 = document.getElementById("box8")
 	  // console.log(box8)
 
+
+
 //=========================================
 //Other Variables
 //=========================================
 	var player = 1; 
-
 	var turnCount = 0;
 
-// document.getElementById("box0").addEventListener("click", function (){
 
-// }
-// document.getElementById("box1").addEventListener("click", function (){
-	
-// }
-// document.getElementById("box2").addEventListener("click", function (){
-	
-// }
-
-
-
-
-    
-
-    
-
-//======================== Settign up the turns ========================
 
 //=========================================
-//Adding click to each div
+//Boxes with onclicks and player turns
 //=========================================
-//Adding event listener vs onclick to be able to change what it is listening for (i.e keystroke)
-
-
-		           // if (box0 == "X") {
-		           // 	box0.removeEventListener("click")
-		           // };
-//======================== Boxes on click... with turns ========================
-
+//======== box0 ========
 	document.getElementById("box0").addEventListener("click", function (){
 		console.log("Box0 was clicked");
 		   if(player == 1 && box0.innerHTML == ""){ //As long as value is not being filled 
-		       box0.innerHTML= "X";
-		       console.log("SAm")
-		       player = 2;
-
+		     box0.innerHTML= "X";
+		     console.log("X works")
+		     player = 2;
 		   }
-
 		   else if(player == 2 && box0.innerHTML == ""){
-		   	box0.innerHTML = "O"
-		   	player = 1
+		   	 box0.innerHTML = "O"
+		   	 player = 1
 		   }
     });
- //    document.getElementById("box1").addEventListener("click", function (){
-	// 	console.log("Box1 was clicked");
-	// 	   if(player == true){
-	// 	       box1.innerHTML= "X"
-	// 	   }
-	// 	   else if(player == false){
-	// 	   	box1.innerHTML = "O"
-	// 	   	player = true
-	// 	   }
- //    });
-	// document.getElementById("box2").addEventListener("click", function (){
-	// 	console.log("Box2 was clicked");
-	// 	   if(player == true){
-	// 	       box2.innerHTML= "X"
-	// 	       player = false
-	// 	   }
-	// 	   else if(player == false){
-	// 	   	box2.innerHTML = "O"
-	// 	   	player = true
-	// 	   }
- //    });
- //    document.getElementById("box3").addEventListener("click", function (){
-	// 	console.log("Box3 was clicked");
-	// 	   if(player == true){
-	// 	       box3.innerHTML= "X"
-	// 	   }
-	// 	   else if(player == false){
-	// 	   	box3.innerHTML = "O"
-	// 	   	player = true
-	// 	   }
- //    });
+//======== box1 ========
+	document.getElementById("box1").addEventListener("click", function (){
+		console.log("Box1 was clicked");
+		   if(player == 1 && box1.innerHTML == ""){ //As long as value is not being filled 
+		     box1.innerHTML= "X";
+		     console.log("X works")
+		     player = 2;
+		   }
+		   else if(player == 2 && box1.innerHTML == ""){
+		   	 box1.innerHTML = "O"
+		   	 player = 1
+		   }
+    });
+//======== box2 ========
+	document.getElementById("box2").addEventListener("click", function (){
+		console.log("Box2 was clicked");
+		   if(player == 1 && box2.innerHTML == ""){ //As long as value is not being filled 
+		     box2.innerHTML= "X";
+		     console.log("X works")
+		     player = 2;
+		   }
+		   else if(player == 2 && box2.innerHTML == ""){
+		   	 box2.innerHTML = "O"
+		   	 player = 1
+		   }
+    });
+//======== box3 ========
+	document.getElementById("box3").addEventListener("click", function (){
+		console.log("Box3 was clicked");
+		   if(player == 1 && box3.innerHTML == ""){ //As long as value is not being filled 
+		     box3.innerHTML= "X";
+		     console.log("X works")
+		     player = 2;
+		   }
+		   else if(player == 2 && box3.innerHTML == ""){
+		   	 box3.innerHTML = "O"
+		   	 player = 1
+		   }
+    });
+//======== box4 ========
+	document.getElementById("box4").addEventListener("click", function (){
+		console.log("Box4 was clicked");
+		   if(player == 1 && box4.innerHTML == ""){ //As long as value is not being filled 
+		     box4.innerHTML= "X";
+		     console.log("X works")
+		     player = 2;
+		   }
+		   else if(player == 2 && box4.innerHTML == ""){
+		   	 box4.innerHTML = "O"
+		   	 player = 1
+		   }
+    });
+//======== box5 ========
+	document.getElementById("box5").addEventListener("click", function (){
+		console.log("Box5 was clicked");
+		   if(player == 1 && box5.innerHTML == ""){ //As long as value is not being filled 
+	       box5.innerHTML= "X";
+		     console.log("X works")
+		     player = 2;
+		   }
+		   else if(player == 2 && box5.innerHTML == ""){
+		   	 box5.innerHTML = "O"
+		   	 player = 1
+		   }
+    });
+//======== box6 ========
+	document.getElementById("box6").addEventListener("click", function (){
+		console.log("Box6 was clicked");
+		   if(player == 1 && box6.innerHTML == ""){ //As long as value is not being filled 
+		     box6.innerHTML= "X";
+		     console.log("X works")
+		     player = 2;
+		   }
+		   else if(player == 2 && box6.innerHTML == ""){
+		     box6.innerHTML = "O"
+		   	 player = 1
+		   }
+    });
+//======== box7 ========
+	document.getElementById("box7").addEventListener("click", function (){
+		console.log("Box7 was clicked");
+		   if(player == 1 && box7.innerHTML == ""){ //As long as value is not being filled 
+		     box7.innerHTML= "X";
+		     console.log("X works")
+		     player = 2;
+		   }
+		   else if(player == 2 && box7.innerHTML == ""){
+		   	 box7.innerHTML = "O"
+		   	 player = 1
+		   }
+    });
+//======== box8 ========
+	document.getElementById("box8").addEventListener("click", function (){
+		console.log("Box8 was clicked");
+		   if(player == 1 && box8.innerHTML == ""){ //As long as value is not being filled 
+		     box8.innerHTML= "X";
+		     console.log("X works")
+		     player = 2;
+		   }
+		   else if(player == 2 && box8.innerHTML == ""){
+		     box8.innerHTML = "O"
+		   	  player = 1
+		   }
+    });
 
 
 	
-
-
-// Possibility 1: while playerTurn1 is equal to true allow them to pick a square and once it is clicked turn them to false. 
-//Possibility 2: If turn count is less than one, allow for some one to click. Once they click they turn false. Then after they select something then they turn false and I turn true. then add one to turn count. 
-
-
-
-
-
-
-// if(turnCount = 0){
-// 	console.log(turnCount)
-// 	playerTurn1 = true	
-// 	playerTurn2 = false
-// 	turnCount = turnCount + 1
-
-// }
 
